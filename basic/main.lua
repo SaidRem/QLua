8787e498b27dc426c5fa548a8be0428ed1b458a3
SaidRem <said.r@yandex.ru>
2021-06-26 22:27:16 +0400

    Added basic

---
+++ basic/main.lua
@@ -0,0 +1,12 @@
+-- main function of the script works in a separate
+-- thread from QUIK.
+-- Even with a heavy load on the QUIK, the main func
+-- will continue to work if there is processor time for QUIK.
+
+function main()
+	-- CODE HERE
+end;
+
+-- If run this script from QUIK then this main() function will be
+-- executed 1 time and the script will stop.
+
