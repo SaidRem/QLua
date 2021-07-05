83b1473643532504056b6ac522691b1188b6907c
SaidRem <said.r@yandex.ru>
2021-07-05 22:56:42 +0400

    Added list of stocks

--- get_data/mmvb_stocks_days.lua
+++ get_data/mmvb_stocks_days.lua
@@ -1,1 +1,10 @@
 -- TODO: Get data from MMVB by days
+
+IS_RUN = true
+
+STOCK_CODES = {"SBER","GAZP","LKOH","GMKN","ALRS","NLMK","ROSN","RUAL","PLZL","VTBR",
+               "YNDX","TATN","FIVE","POLY","CHMF","AFKS","MOEX","NVTK","AFLT","SNGS",
+	       "MAGN","OZON","IRAO","SBERP","TCSG","SNGSP","MGNT","MTSS","MAIL","SIBN",
+	       "TATNP","PHOR","TRNFP","CBOM","RTKM","HYDR","RASP","POGR","FEES","MVID",
+	       "FLOT","DSKY","OGKB","RSTI","UPRO","ENPG","PIKK","BANEP",}
+
