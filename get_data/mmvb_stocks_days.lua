e88da3622329ac609fc061361b1316c93f1e761c
SaidRem <said.r@yandex.ru>
2021-07-06 22:36:30 +0400

    Get days data from quik

--- get_data/mmvb_stocks_days.lua
+++ get_data/mmvb_stocks_days.lua
@@ -7,4 +7,69 @@ STOCK_CODES = {"SBER","GAZP","LKOH","GMKN","ALRS","NLMK","ROSN","RUAL","PLZL","V
 	       "MAGN","OZON","IRAO","SBERP","TCSG","SNGSP","MGNT","MTSS","MAIL","SIBN",
 	       "TATNP","PHOR","TRNFP","CBOM","RTKM","HYDR","RASP","POGR","FEES","MVID",
 	       "FLOT","DSKY","OGKB","RSTI","UPRO","ENPG","PIKK","BANEP",}
+PERIOD = INTERVAL_D1
+correct_time = 3
+filename = "E:\\data"
+BARS = 250
 
+function strText(i)
+    local m = tostring(i)
+    local mLen = string.len(i)
+
+    if mLen == 1 then
+        res = "0" .. m
+    else res = m
+    end
+    return res
+end
+
+function main()
+    for i = 1, #STOCK_CODES do
+        BARS = 250
+        instrument = STOCK_CODES[i]
+        out_file = io.open(filename.."\\"..STOCK_CODES[i]..".csv", "w")
+        -- out_file_info = io.open(filename.."\\"..STOCK_CODES[i].."_info.csv", "w")
+        -- info_table = getSecurityInfo("TQBR", STOCK_CODES[i])
+        ds = CreateDataSource("TQBR", instrument, PERIOD)
+        ds:SetEmptyCallback()
+        -- ds:SetUpdateCallback(NewChartData)
+
+        local Size = ds:Size()
+        local x = 0
+        if Size == 0 then
+            while x < 10 and Size == 0 do
+                Size = ds:Size()
+                x = x + 1
+                -- message('Waiting for '..instrument)
+                sleep(250)
+            end
+        end
+
+        if BARS > Size then
+            BARS = Size - 1
+        end
+
+        for i = Size - BARS, Size, 1 do
+            local O = ds:O(i)
+            local H = ds:H(i)
+            local L = ds:L(i)
+            local C = ds:C(i)
+            local V = ds:V(i)
+            local T = ds:T(i)
+            sTime = os.time(T)
+            datetime = os.date("!*t", sTime)
+                
+            out_file:write(instrument..","..tostring(PERIOD)..","..tostring(datetime.year)..tostring(strText(datetime.month))
+                           ..tostring(strText(datetime.day))..","..tostring(strText(datetime.hour + correct_time))
+                           ..tostring(strText(datetime.min))..tostring(strText(datetime.sec))..","..tostring(O)..","
+                           ..tostring(H)..","..tostring(L)..","..tostring(C)..","..tostring(V).."\n")
+            out_file:flush()  -- Write data to the file.
+
+        end
+
+        out_file:close()
+        ds:Close()
+        sleep(20)  -- stop for 0.02 sec
+    end
+
+end
